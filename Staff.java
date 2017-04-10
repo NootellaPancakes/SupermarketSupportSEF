39637165efb22961aac9490ae32fc818463f7ad0
rmit-s3581721-ZhiPeng-Li <s3581721@student.rmit.edu.au>
2017-04-10 23:55:37 +1000

    Sales staff & WareHouseStaff

---
+++ Staff.java
@@ -0,0 +1,18 @@
+
+public class Staff extends User {
+	public Staff(String userID, String name, String password, String dateOfBirth, int phoneNumber, String email){
+		super( userID, name , password , dateOfBirth , phoneNumber , email);
+	}
+		public String getID(){
+			super.getID();
+		}
+		public void setID(){
+			super.setID();
+		}
+		public void cancelCustomerOrder(){
+			
+		}
+		public void undoCustomerSelection(){
+				
+		}
+}
---
+++ WareHouseStaff.java
@@ -0,0 +1,18 @@
+
+public class WareHouseStaff extends User {
+	public WareHouseStaff(String userID, String name, String password, String dateOfBirth, int phoneNumber, String email){
+		super( userID, name , password , dateOfBirth , phoneNumber , email);
+	}
+		public String getID(){
+			super.getID();
+		}
+		public void setID(){
+			super.setID();
+		}
+		public void replenishStock(){
+			
+		}
+		public void checkStock(){
+				
+		}
+}
